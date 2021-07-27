dd82b18a02b38c85baba6bb4e625af09a3747186
oscasand <oscasand@gmail.com>
2021-07-27 08:59:22 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, MoviesComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
